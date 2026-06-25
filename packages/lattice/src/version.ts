8d42eadb3771ccdf4830ef11f111076d53157efb
LakshmanTurlapati <lakshmantvnm@gmail.com>
2026-06-25 04:50:58 -0500

    chore: re-stamp latticeVersion from package.json (1.3.0 -> 1.4.0)

--- packages/lattice/src/version.ts
+++ packages/lattice/src/version.ts
@@ -1,4 +1,4 @@
 // AUTO-GENERATED FILE - DO NOT EDIT.
 // Source: package.json version field.
 // Regenerate with this package's stamp:version script.
-export const latticeVersion = "1.3.0";
+export const latticeVersion = "1.4.0";
